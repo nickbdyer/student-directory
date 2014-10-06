675a0c25f7a17f95d27d7efa7b71673202338aa6
Nick Dyer <nbdyer@gmail.com>
2014-10-06 11:36:31 +0100

    Changed to array

--- directory.rb
+++ directory.rb
@@ -1,64 +1,126 @@
 student_count = 57
+# array of students
+students = [
+ "John Kiely",
+ "Chris Batts",
+ "Ben Hutchinson",
+ "Ayo Obembe",
+ "Mishal",
+ "Chris Handley",
+ "Giorgia Amici",
+ "Attila",
+ "Pavel Redics",
+ "Janacuq",
+ "Edward Byne",
+ "Arfah",
+ "langesi",
+ "Richard Lake",
+ "CZC8190BMC",
+ "Nick Dyer",
+ "Izzy Markwick",
+ "Alan Bridger",
+ "Alex Blease",
+ "Shan Hasan",
+ "Claudia Callisto",
+ "Dale Stevens",
+ "Abdul Hassan",
+ "Alan Bridger",
+ "Alexander Levin",
+ "Ana Nogal",
+ "Andrew Harrison",
+ "Andrew Hercules",
+ "Andrew Snead",
+ "Anna Jerofejeva",
+ "Bernard Mordan",
+ "Camilla van Klinken",
+ "Clare Trembath",
+ "Colin Smif",
+ "Contra Mundi",
+ "Craig Horsborough",
+ "Daniel Cutler",
+ "Daniel Le Dosquet",
+ "Elena Garrone",
+ "Ella Schofield",
+ "Fad Fad",
+ "Jana Sanchez",
+ "James Carter",
+ "Johnny Lakin",
+ "Karin Nielsen",
+ "Marcherita Serino",
+ "Ming Chan",
+ "Nicole Pellicena",
+ "Nikesh Ashar",
+ "Sandrine Zhang-Ferron",
+ "Shan Hasan",
+ "Simon Lange",
+ "Stephen Giles",
+ "Tim Scully",
+ "Victoria Stepanovna",
+ "Yvette Cook",
+ "Zeeshan Rasool"
+]
 # first we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "------------"
-puts "John Kiely"
-puts "Chris Batts"
-puts "Ben Hutchinson"
-puts "Ayo Obembe"
-puts "Mishal"
-puts "Chris Handley"
-puts "Giorgia Amici"
-puts "Attila"
-puts "Pavel Redics"
-puts "Janacuq"
-puts "Edward Byne"
-puts "Arfah"
-puts "langesi"
-puts "Richard Lake"
-puts "CZC8190BMC"
-puts "Nick Dyer"
-puts "Izzy Markwick"
-puts "Alan Bridger"
-puts "Alex Blease"
-puts "Shan Hasan"
-puts "Claudia Callisto"
-puts "Dale Stevens"
-puts "Abdul Hassan"
-puts "Alan Bridger"
-puts "Alexander Levin"
-puts "Ana Nogal"
-puts "Andrew Harrison"
-puts "Andrew Hercules"
-puts "Andrew Snead"
-puts "Anna Jerofejeva"
-puts "Bernard Mordan"
-puts "Camilla van Klinken"
-puts "Clare Trembath"
-puts "Colin Smif"
-puts "Contra Mundi"
-puts "Craig Horsborough"
-puts "Daniel Cutler"
-puts "Daniel Le Dosquet"
-puts "Elena Garrone"
-puts "Ella Schofield"
-puts "Fad Fad"
-puts "Jana Sanchez"
-puts "James Carter"
-puts "Johnny Lakin"
-puts "Karin Nielsen"
-puts "Marcherita Serino"
-puts "Ming Chan"
-puts "Nicole Pellicena"
-puts "Nikesh Ashar"
-puts "Sandrine Zhang-Ferron"
-puts "Shan Hasan"
-puts "Simon Lange"
-puts "Stephen Giles"
-puts "Tim Scully"
-puts "Victoria Stepanovna"
-puts "Yvette Cook"
-puts "Zeeshan Rasool"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+puts students[11]
+puts students[12]
+puts students[13]
+puts students[14]
+puts students[15]
+puts students[16]
+puts students[17]
+puts students[18]
+puts students[19]
+puts students[20]
+puts students[21]
+puts students[22]
+puts students[23]
+puts students[24]
+puts students[25]
+puts students[26]
+puts students[27]
+puts students[28]
+puts students[29]
+puts students[30]
+puts students[31]
+puts students[32]
+puts students[33]
+puts students[34]
+puts students[35]
+puts students[36]
+puts students[37]
+puts students[38]
+puts students[39]
+puts students[40]
+puts students[41]
+puts students[42]
+puts students[43]
+puts students[44]
+puts students[45]
+puts students[46]
+puts students[47]
+puts students[48]
+puts students[49]
+puts students[50]
+puts students[51]
+puts students[52]
+puts students[53]
+puts students[54]
+puts students[55]
+puts students[56]
+puts students[57]
+
 #finally, we print the total
 puts "Overall, we have #{student_count} great students"
 
