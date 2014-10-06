54ad2192c783507cffd7880d50ac38dbe07b66bb
Nick Dyer <nbdyer@gmail.com>
2014-10-06 11:38:47 +0100

    Added iterator and removed student count variable

--- directory.rb
+++ directory.rb
@@ -1,4 +1,3 @@
-student_count = 57
 # array of students
 students = [
  "John Kiely",
@@ -62,67 +61,12 @@ students = [
 # first we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts students[11]
-puts students[12]
-puts students[13]
-puts students[14]
-puts students[15]
-puts students[16]
-puts students[17]
-puts students[18]
-puts students[19]
-puts students[20]
-puts students[21]
-puts students[22]
-puts students[23]
-puts students[24]
-puts students[25]
-puts students[26]
-puts students[27]
-puts students[28]
-puts students[29]
-puts students[30]
-puts students[31]
-puts students[32]
-puts students[33]
-puts students[34]
-puts students[35]
-puts students[36]
-puts students[37]
-puts students[38]
-puts students[39]
-puts students[40]
-puts students[41]
-puts students[42]
-puts students[43]
-puts students[44]
-puts students[45]
-puts students[46]
-puts students[47]
-puts students[48]
-puts students[49]
-puts students[50]
-puts students[51]
-puts students[52]
-puts students[53]
-puts students[54]
-puts students[55]
-puts students[56]
-puts students[57]
+students.each do |student|
+	puts student
+end
 
 #finally, we print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
 
 
 
