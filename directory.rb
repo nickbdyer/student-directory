9881b7abb4841055678f9c4c0706c7f904a10e44
Nick Dyer <nbdyer@gmail.com>
2014-10-07 15:43:23 +0100

    Refactored interactive menu, and added ternary to pluralisation instead of if.

--- directory.rb
+++ directory.rb
@@ -1,38 +1,35 @@
 require 'Date'
 
+@students = []
+
 #first we print the header
 def print_header
-	puts "The students of my cohort at Makers Academy".center(150)
-	puts "------------".center(150)
+	puts "The students of my cohort at Makers Academy".center(100)
+	puts "------------".center(100)
 end
 
 # unsorted list of students
-def print_list(students)
-	i = 0
-	while i < students.length do
-			puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort), hobbies include #{students[i][:hobbies]} and being born on #{students[i][:birthplace]}.".center(150)
-			i+=1
+def print_student_list
+	@students.each_with_index do |student, index|
+		puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort), hobbies include #{student[:hobbies]} and being born on #{student[:birthplace]}.".center(100)
 	end
 end
 
 # list of students sorted by month
-def print_sorted_list(students)
-	sorted_students = students.sort_by {|student| Date::MONTHNAMES.index("#{student[:cohort]}")}
+def print_sorted_list
+	sorted_students = @students.sort_by {|student| Date::MONTHNAMES.index("#{student[:cohort]}")}
 	sorted_students.each do |student| 
-		puts "#{student[:name]} (#{student[:cohort]} cohort), hobbies include #{student[:hobbies]} and being born on #{student[:birthplace]}.".center(150)
+		puts "#{student[:name]} (#{student[:cohort]} cohort), hobbies include #{student[:hobbies]} and being born on #{student[:birthplace]}.".center(100)
 	end
 end
 
 #finally, we print the total
-def print_footer(names) 
-	if names.length == 1
-	puts "Overall, we have #{names.length} great student".center(150)
-	else
-	puts "Overall, we have #{names.length} great students".center(150)
-	end
+def print_footer
+	last_word = @students.length == 1 ? "student" : "students"
+	puts "Overall, we have #{@students.length} great #{last_word}.".center(100)
 end
 
-def input_students(students)
+def input_students
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
 	#get the first name
@@ -52,64 +49,66 @@ def input_students(students)
 				input = gets.chomp.to_i
 			end
 		end
-		students << {:name => name.capitalize, :cohort => cohort.to_sym, :hobbies => :coding, :birthplace => :earth}
-		if students.length == 1
-			puts "Now we have #{students.length} student"
-		else
-			puts "Now we have #{students.length} students"
-		end
+		@students << {:name => name.capitalize, :cohort => cohort.to_sym, :hobbies => :coding, :birthplace => :earth}
+		last_word = @students.length == 1 ? "student" : "students"
+			puts "Now we have #{@students.length} #{last_word}"
 		# get another name from the user
 		name = gets.chomp
 	end
 	#return the array of students
-	students
+	@students
 end
 
-def interactive_menu
-	students = []
-	loop do
-		#1. Print Menu, and ask user what they want to do. 
-		puts "1. Input the students."
-		puts "2. Show the student list."
-		puts "9. Exit"
-		#2. Ask the user for input. 
-		selection = gets.chomp
-		#3. Do what the user asked. 
-		case selection
+
+def print_menu
+	puts "1. Input the students."
+	puts "2. Show the student list."
+	puts "9. Exit"
+end
+
+
+def show_students
+	if !@students.empty?
+		puts "Sort by cohort?(y/n)"
+		input = gets.chomp
+		print_header
+		case input
+		when "y"
+			print_sorted_list
+		when "n"
+			print_student_list
+		else
+			print_student_list
+		end
+		print_footer
+	else
+		puts "There are no students stored in the system."
+	end
+end
+
+
+def process(selection)
+	case selection
 		when "1"
-			students = input_students(students)
+			input_students
 		when "2"
-			if !students.empty?
-				puts "Sort by cohort?(y/n)"
-				input = gets.chomp
-				case input
-				when "y"
-					print_header
-					print_sorted_list(students)
-					print_footer(students)
-				when "n"
-					print_header
-					print_list(students)
-					print_footer(students)
-				else
-					print_header
-					print_list(students)
-					print_footer(students)
-				end
-			else
-				puts "There are no students stored in the system."
-			end
+			show_students
 		when "9"
-			Exit
+			exit
 		else
 			puts "I dont' know what you meant, try again."
-		end
+	end
+end
+
+
+def interactive_menu
+	loop do
+		print_menu
+		process(gets.chomp)
 	end
 end
 
 interactive_menu
-
-
 
 
 
